0cdb1846781e69127d3947804dd1da22583c6c7e
ricekeet <ninjabasketball@hotmail.com>
2021-12-11 05:09:04 -0500

    added button functions on home menu

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,4 +20,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void btnHandler(View view) {
+        switch (view.getId()){
+            case(R.id.btnReminderList):{
+                startActivity(new Intent(this,Reminders.class));
+                break;
+            }
+            case(R.id.btnSettings):{
+                startActivity(new Intent(this,Settings.class));
+                break;
+            }
+            case(R.id.btnAbout):{
+                break;
+            }
+            case(R.id.btnWidgets):{
+                break;
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/finalproject/Reminders.java
+++ app/src/main/java/com/example/finalproject/Reminders.java
@@ -2,7 +2,9 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Reminders extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class Reminders extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders);
+    }
+
+    public void btnHandler(View view) {
+        switch(view.getId()){
+            case(R.id.btnRemindersBack):{
+                startActivity(new Intent(this,MainActivity.class));
+            }
+            case(R.id.btnRemindersAdd):{
+                break;
+            }
+        }
     }
 }
