fa1c2f303dc248b6a0540fb7ba3044e238fcaabd
ricekeet <ninjabasketball@hotmail.com>
2021-12-11 05:27:56 -0500

    added button functionality to the back button in Settings. Added Darkmode backgrounds

--- app/src/main/java/com/example/finalproject/Settings.java
+++ app/src/main/java/com/example/finalproject/Settings.java
@@ -2,7 +2,9 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Settings extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    public void btnHandler(View view) {
+        if (view.getId() == R.id.btnSettingsBack){
+            startActivity(new Intent(this, MainActivity.class));
+        }
     }
 }
