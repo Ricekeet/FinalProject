106c3b5e40a7fd567df5bbbbb6c3c5ba54e0831d
ricekeet <ninjabasketball@hotmail.com>
2021-12-11 12:57:35 -0500

    Changed Settings Activity, updated pic names

--- app/src/main/java/com/example/finalproject/Settings.java
+++ app/src/main/java/com/example/finalproject/Settings.java
@@ -1,22 +1,39 @@
 package com.example.finalproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
 
 public class Settings extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        setContentView(R.layout.settings_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.settings, new SettingsFragment())
+                    .commit();
+        }
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
     public void btnHandler(View view) {
-        if (view.getId() == R.id.btnSettingsBack){
-            startActivity(new Intent(this, MainActivity.class));
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
         }
     }
 }
